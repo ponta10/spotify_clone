f360c9c3d2f8b3f09808da5fc0f22c21b7dda3eb
ponta10 <mponta2002@gmail.com>
2023-06-05 23:55:51 +0900

    make layout

---
+++ app/(site)/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <p className='text-green-500'>Main</p>
+  )
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname === "/",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    [pathname]
+  );
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item?.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Sidebar Navigaton</Box>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
